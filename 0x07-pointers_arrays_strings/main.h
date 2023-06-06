e3e11efa276b76daa254c4bf17cfea0be1ab3f92
Amazingkemi <elizabethoarogundade@gmail.com>
2023-06-06 13:41:02 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+char *_strchr(char *s, char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+char *_strstr(char *haystack, char *needle);
+void print_diagsums(int *a, int size);
+
+#endif
