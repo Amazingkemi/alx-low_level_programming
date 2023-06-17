47f9b5364db089c5dd9f4563f7ac064147c964b0
Amazingkemi <elizabethoarogundade@gmail.com>
2023-06-17 07:40:12 -0700

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the number
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all arguement it receives
+ * @argv: arguement vector
+ * @argc: arguement count
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+return (0);
+}
