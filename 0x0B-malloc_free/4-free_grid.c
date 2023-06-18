fdd2d72ad49f5abbf4677579ce018449602b1a08
Amazingkemi <elizabethoarogundade@gmail.com>
2023-06-18 11:37:31 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,12 +8,13 @@
  */
 void free_grid(int **grid, int height)
 {
-        int i;
-        i = 0;
-        while (i < height)
-        {
-                free(grid[i]);
-                i++;
-        }
-free(grid);
+	int i;
+
+	i = 0;
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
 }
